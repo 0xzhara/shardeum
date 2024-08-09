7b2f943888bc89b9574e4df378bd75c94853d69f
0xzhara.eth <122523974+0xzhara@users.noreply.github.com>
2024-08-09 17:11:29 +0700

    Update install_shardeum.sh

--- install_shardeum.sh
+++ install_shardeum.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Display logo and message
 curl -s https://raw.githubusercontent.com/zrhraJETTOKOSUTA/bash-nobi.sh/main/bash%20logo.sh | bash
 echo "Join the Airdrop Nobi Telegram channel: https://t.me/airdropnobi"
 sleep 4
@@ -9,13 +10,6 @@ sudo apt update
 
 # Install curl and Docker
 sudo apt install -y curl docker.io
-
-# Install Homebrew (if not already installed)
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# Add Homebrew to PATH
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.bashrc
-eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Install Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
